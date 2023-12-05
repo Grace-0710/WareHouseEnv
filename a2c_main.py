6c652cf363d50d5ee191e478ab5a726b8cf6924f
grace <sg04096@gmail.com>
2023-12-05 16:32:30 +0900

    no message

--- a2c_main.py
+++ a2c_main.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from env import WareHouseEnv
 import warnings
 import matplotlib.pyplot as plt
+from datetime import datetime
 # 경고 메시지 무시 설정
 warnings.filterwarnings('ignore', category=UserWarning, module='stable_baselines3.common.vec_env.base_vec_env')
 
@@ -12,16 +13,16 @@ import time
 # [ExcavatorEnv 코드는 여기에 위치]
  
 # 환경 생성
-env = DummyVecEnv([lambda: WareHouseEnv(map_size=4, max_steps=5000, graphic=0, fps=30)])
+env = DummyVecEnv([lambda: WareHouseEnv(map_size=4, max_steps=5000, graphic=0, fps=150)])
 
 # A2C 모델 초기화
 model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=100_000_000)
+model.learn(total_timesteps=100_000)
 
 # 학습된 모델을 환경에서 테스트
 obs = env.reset()
 reward_return_list = []
-for _ in range(100_000_000):
+for _ in range(100_000):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     reward_return_list.append(rewards)
@@ -29,9 +30,11 @@ for _ in range(100_000_000):
 
 
 env.close()
+now = datetime.now()
+nowtxt = now.strftime('%Y-%m-%d%H:%M:%S')
 plt.plot(reward_return_list)
 plt.xlabel('Iteration')
 plt.ylabel('Reward A2C')
-plt.savefig('Reward_A2C.png', format='png', dpi=300)
+plt.savefig('Reward_A2C.png'+nowtxt+'.png', format='png', dpi=300)
 # Display the plot
 plt.show()
--- dqn_main.py
+++ dqn_main.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from env import WareHouseEnv
 import warnings
 import matplotlib.pyplot as plt
+from datetime import datetime
 # 경고 메시지 무시 설정
 warnings.filterwarnings('ignore', category=UserWarning, module='stable_baselines3.common.vec_env.base_vec_env')
 
@@ -12,7 +13,7 @@ import time
 # [ExcavatorEnv 코드는 여기에 위치]
  
 # 환경 생성
-env = DummyVecEnv([lambda: WareHouseEnv(map_size=4, max_steps=5000, graphic=1, fps=150)])
+env = DummyVecEnv([lambda: WareHouseEnv(map_size=4, max_steps=5000, graphic=0, fps=150)])
 
 def learning_rate_schedule(progress_remaining):
     # 이 함수는 학습의 진행에 따라 학습률을 반환합니다.
@@ -32,10 +33,11 @@ for _ in range(100_000):
 
 
 env.close()
-
+now = datetime.now()
+nowtxt = now.strftime('%Y-%m-%d%H:%M:%S')
 plt.plot(reward_return_list)
 plt.xlabel('Iteration')
 plt.ylabel('Reward DQN')
-plt.savefig('Reward_DQN.png', format='png', dpi=300)
+plt.savefig('Reward_DQN.png'+nowtxt+'.png', format='png', dpi=300)
 # Display the plot
 plt.show()
